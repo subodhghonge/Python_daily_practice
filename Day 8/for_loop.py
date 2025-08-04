59efb6d1d250496255217166a9b8d847c53d0b32
subodhghonge <subodhghonge12@gmail.com>
2025-08-04 17:03:20 +0530

    for loop programs

--- Day 8/for_loop.py
+++ Day 8/for_loop.py
@@ -1,16 +1,16 @@
-# #1 to display 1 - 10 numbers
-# for i in range(1, 11, 1):
-#     print(i)
+#1 to display 1 - 10 numbers
+for i in range(1, 11, 1):
+    print(i)
 
-# #2 to display even numbers between 1 to 20
-# for i in range(1, 21, 1):
-#     if i % 2 == 0:
-#         print("Even Numbers Are : ", i)
+#2 to display even numbers between 1 to 20
+for i in range(1, 21, 1):
+    if i % 2 == 0:
+        print("Even Numbers Are : ", i)
 
-# #3 to dislay odd numbers between 1 to 20
-# for i in range(1, 21, 1):
-#     if i % 2 != 0:
-#         print("Odd Numbers Are : ", i)  
+#3 to dislay odd numbers between 1 to 20
+for i in range(1, 21, 1):
+    if i % 2 != 0:
+        print("Odd Numbers Are : ", i)  
 
 #4 to display 10 to 1 numbers
 for i in range(10, 0, -1):
---
+++ Day 8/square_&_cube.py
@@ -0,0 +1,8 @@
+#1 To display square of 1 to 10 numbers
+
+for i in range(1,11):
+    print(f"Square of {i} is: ", i**2)
+
+#2 To display cube of 1 to 10 numbers
+for i in range(1, 11):
+    print(f"Cube of {i} is: ", i**3)
