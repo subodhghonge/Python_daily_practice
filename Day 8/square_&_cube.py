304ed8b7fe7e83efeaa37c61fb3d73f67a79eaa3
subodhghonge <subodhghonge12@gmail.com>
2025-08-04 17:14:49 +0530

    For update

--- Day 8/square_&_cube.py
+++ Day 8/square_&_cube.py
@@ -6,3 +6,10 @@ for i in range(1,11):
 #2 To display cube of 1 to 10 numbers
 for i in range(1, 11):
     print(f"Cube of {i} is: ", i**3)
+
+#3 to display square of even no and cube of odd numbers
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(f"Square of {i} : ", i**2)
+    else:
+        print(f"Square of {i} : ", i**3)
---
+++ Day 8/sum_of_numbers.py
@@ -0,0 +1,5 @@
+# to calculate sum of 1 to 10 numbers.
+sum = 0
+for i in range(1, 11):
+    sum+=i
+print(sum)
