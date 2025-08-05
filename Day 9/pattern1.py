927c93d6da4c68e465d622eeeaaecce7b4842828
subodhghonge <subodhghonge12@gmail.com>
2025-08-05 17:36:17 +0530

    Basic Patterns

---
+++ Day 9/pattern1.py
@@ -0,0 +1,35 @@
+# #1) to print the pattern using for loop
+# # * * * * *
+# # * * * * *
+# # * * * * *
+# # * * * * *
+# # * * * * *
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print("*", end=" ")
+#     print(end="\n")
+
+# #2) to print the pattern using for loop
+# # 1 1 1 1 1
+# # 2 2 2 2 2
+# # 3 3 3 3 3
+# # 4 4 4 4 4
+# # 5 5 5 5 5
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(i, end=" ")
+#     print()
+
+# #3) to print the pattern using for loop
+# # 1 2 3 4 5
+# # 1 2 3 4 5
+# # 1 2 3 4 5
+# # 1 2 3 4 5
+# # 1 2 3 4 5
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(j, end=" ")
+#     print()
