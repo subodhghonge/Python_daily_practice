6d1ef7221c34d419164f15c1a0a052158b61d3e4
subodhghonge <subodhghonge12@gmail.com>
2025-09-03 20:02:43 +0530

    Strings

--- Strings Concept/basic.py
+++ Strings Concept/basic.py
@@ -1,13 +1,54 @@
-#1. Reverse a string without slicing.
+# #1. Reverse a string without slicing.
 
-n = input("Enter a string to reverse : ")
+# n = input("Enter a string to reverse : ")
 
-for i in range(len(n)-1, -1, -1):
-    print(n[i], end="")
-print()
+# for i in range(len(n)-1, -1, -1):
+#     print(n[i], end="")
+# print()
 
-#2. Reverse a string with slicing method
-n = input("Enter a string to reverse : ")
+# #2. Reverse a string with slicing method
+# n = input("Enter a string to reverse : ")
 
-print(n[::-1])
+# print(n[::-1])
 
+# #3. Check if string is palindrome.
+# def palindrome(n):
+#     reverse = ""
+#     for i in range(len(n)-1, -1, -1):
+#         reverse = reverse + n[i]
+#     return reverse
+
+# n = input("Enter a string to reverse : ")
+
+# if n == palindrome(n):
+#     print("String is palindrome")
+# else:
+#     print("string is not palindrome")
+
+#4. Count vowels, consonants, digits, special chars.
+def count_char(s):
+    vowels = 'aeiou'
+
+    v_c = c_c = d_c = s_c = 0
+
+    for i in n:
+        if i.isalpha():
+            if i in vowels:
+                v_c += 1
+            else:
+                c_c += 1
+        elif i.isdigit():
+            d_c += 1
+        else:
+            s_c += 1
+    
+    return v_c, c_c, d_c, s_c
+
+n = input("Enter your email Id :- ")
+
+v, c, d, s = count_char(n)
+
+print("vowels count is : ", v)
+print("vowels count is : ", c)
+print("vowels count is : ", d)
+print("vowels count is : ", s)
