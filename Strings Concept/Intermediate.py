dd1a74696c3b7c8bd262cb2134d3a922b778b160
subodhghonge <subodhghonge12@gmail.com>
2025-09-06 19:39:42 +0530

    Intermediate Programs

--- Strings Concept/Intermediate.py
+++ Strings Concept/Intermediate.py
@@ -1,1 +1,7 @@
 #1. Remove duplicates from a string (programming → progamin).
+n = input("Enter a String: ")
+result = ""
+for i in n:
+    if i not in result:
+        result = result + i
+print(result)
