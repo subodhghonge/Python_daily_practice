15f6b6a866387ef31907caeafb7040b5fa84069d
subodhghonge <subodhghonge12@gmail.com>
2025-09-07 15:47:08 +0530

    Intermediate Programs

--- Strings Concept/Intermediate.py
+++ Strings Concept/Intermediate.py
@@ -1,78 +1,110 @@
-# #1. Remove duplicates from a string (programming → progamin).
-# n = input("Enter a String: ")
-# result = ""
-# for i in n:
-#     if i not in result:
-#         result = result + i
-# print(result)
+#1. Remove duplicates from a string (programming → progamin).
+n = input("Enter a String: ")
+result = ""
+for i in n:
+    if i not in result:
+        result = result + i
+print(result)
 
-# #2. First non-repeating character in a string
-# n = input("Enter a String: ")
-# freq = {}
+#2. First non-repeating character in a string
+n = input("Enter a String: ")
+freq = {}
 
-# for ch in n:
-#     freq[ch] = freq.get(ch, 0) + 1
+for ch in n:
+    freq[ch] = freq.get(ch, 0) + 1
 
-# for ch in n:
-#     if freq[ch] == 1:
-#         print(ch)
+for ch in n:
+    if freq[ch] == 1:
+        print(ch)
 
-# #3.Check if two strings are anagrams (listen, silent).
-# a = input("Enter string1 : ")
-# b = input("Enter string2 : ")
+#3.Check if two strings are anagrams (listen, silent).
+a = input("Enter string1 : ")
+b = input("Enter string2 : ")
 
-# if sorted(a.lower()) == sorted(b.lower()):
-#     print("Anagram")
-# else:
-#     print("Not Anagram")
+if sorted(a.lower()) == sorted(b.lower()):
+    print("Anagram")
+else:
+    print("Not Anagram")
 
-# #4. Find all substrings of a string.
-# def sub_str(s):
-#     substr = []
-#     n = len(s)
+#4. Find all substrings of a string.
+def sub_str(s):
+    substr = []
+    n = len(s)
 
-#     for i in range(n):
-#         for j in range(i+1, n+1):
-#             substr.append(s[i:j])
+    for i in range(n):
+        for j in range(i+1, n+1):
+            substr.append(s[i:j])
     
-#     return substr
+    return substr
 
-# i = input("Enter a string: ")
+i = input("Enter a string: ")
 
-# result = sub_str(i)
+result = sub_str(i)
 
-# print(result)
+print(result)
 
-# #5. Find longest word in a sentence.
-# def long_word(str):
-#     word = str.split()
-#     longest = ""
+#5. Find longest word in a sentence.
+def long_word(str):
+    word = str.split()
+    longest = ""
 
-#     for i in word:
-#         if len(i) > len(longest):
-#             longest = i
+    for i in word:
+        if len(i) > len(longest):
+            longest = i
     
-#     return longest
+    return longest
 
-# i = input("Enter a sentence: ")
+i = input("Enter a sentence: ")
 
-# result = long_word(i)
-# print(result)
+result = long_word(i)
+print(result)
 
-# #6. Replace spaces with - (URL formatting).
-# def spaces(s):
-#     result = ""
+#6. Replace spaces with - (URL formatting).
+def spaces(s):
+    result = ""
 
-#     for i in s:
-#         if i == " ":
-#             result += '-'
-#         else:
-#             result += i
+    for i in s:
+        if i == " ":
+            result += '-'
+        else:
+            result += i
     
-#     return result
+    return result
 
-# i = input("Enter a string: ")
+i = input("Enter a string: ")
 
-# r = spaces(i)
+r = spaces(i)
 
-# print("String : ", r)
+print("String : ", r)
+
+#7. Check if a string has all unique characters
+# using set function
+def unqiue(s):
+    return len(s) == len(set(s))
+
+i = input("Enter a string : ")
+
+if unqiue(i):
+    print("All Character are unique")
+else:
+    print("All Characters are not unqiue")
+
+#8. Count frequency of each word in a sentence.
+def freq(s):
+    word = s.split()
+    freq = {}
+
+    for i in word:
+        i = i.lower()
+        freq[i] = freq.get(i, 0)+1
+    
+    return freq
+
+i = input("Enter a string : ")
+d = {}
+result = freq(i)
+for word, count in result.items():
+    l = {word:count}
+    d.update(l)
+
+print(d)
