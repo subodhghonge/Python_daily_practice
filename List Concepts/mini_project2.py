55d6d40045f132aac57116a1e1afa92d25f27bc8
subodhghonge <subodhghonge12@gmail.com>
2025-09-15 21:40:04 +0530

    Shopping Cart

---
+++ List Concepts/mini_project2.py
@@ -0,0 +1,32 @@
+#Shopping Cart
+def shopping_card():
+    cart = []
+
+    while True:
+        print("\nShopping Cart Menu:")
+        print("1. Add item")
+        print("2. Remove item")
+        print("3. View cart")
+        print("4. Exit")
+        ch = int(input("Enter choice: "))
+
+        if ch == 1:
+            item = input("Enter item to add: ")
+            cart.append(item)
+            print(f"{item} added to cart succesfully!")
+        elif ch == 2:
+            item = input("Enter the item to remove from cart: ")
+            if item in cart:
+                cart.remove(item)
+            else:
+                print("Item not found in cart")
+            print(f"{item} removed from cart succesfully!")
+        elif ch == 3:
+            print(f"Cart items {cart}")
+        elif ch == 4:
+            print("Exiting......Thaank you!")
+            break
+        else:
+            print("Enter a valid number")
+
+shopping_card()
