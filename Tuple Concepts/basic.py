a3f96f275e8c8c24abf2c1b7c6ba38291c5bc080
subodhghonge <subodhghonge12@gmail.com>
2025-09-16 22:46:29 +0530

    Basic Programs

---
+++ Tuple Concepts/basic.py
@@ -0,0 +1,33 @@
+# #1. Create a tuple with integers, strings, and mixed types.
+# a = (1, "John", "Washington", 65.40)
+
+# #2. Find length of tuple without using len().
+# b = ('apple', 'banana','cherry','dragonfruit','banana', 'mango','watermelon')
+
+# print(len(a))
+
+# #3. Check if an element exists in tuple.
+
+# tuplee = (1, "John", "Washington", 65.40)
+
+# element = input("Enter the element to search:")
+
+# if element in tuplee:
+#     print("Element present")
+# else:
+#     print("Not present")
+
+#4. Convert tuple → list and list → tuple.
+x = ('apple', 'banana','cherry','dragonfruit','banana', 'mango','watermelon')
+
+#5. Slice a tuple (first 3, last 3, alternate elements).
+
+#6. Reverse a tuple.
+
+#7. Count frequency of an element in tuple.
+
+#8. Find index of an element in tuple.
+
+#9. Concatenate two tuples.
+
+#10. Repeat a tuple 5 times.
