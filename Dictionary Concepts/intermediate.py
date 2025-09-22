06695798dd850fc2e3437820c489e3216727a123
subodhghonge <subodhghonge12@gmail.com>
2025-09-22 18:33:11 +0530

    Intermediate Programs

---
+++ Dictionary Concepts/intermediate.py
@@ -0,0 +1,48 @@
+# #1. Count frequency of each character in a string.
+# str = "hello world!. welcome to python programming"
+# freq = {}
+
+# for i in str:
+#     if i in freq:
+#         freq[i] += 1
+#     else:
+#         freq[i] = 1
+
+# print(freq)
+
+# #2. Count frequency of each word in a sentence.
+# str = "hello world!. welcome to python programming"
+
+# words = str.split()
+
+# freq = {}
+
+# for i in words:
+#     if i in freq:
+#         freq[i] += 1
+#     else:
+#         freq[i] = 1
+
+# print(freq)
+
+# #3. Find key with maximum value.
+# dict = {'a':10, 'b':20, 'c':30, 'd':40}
+
+# for key in dict:
+#     if dict[key] == max(dict.values()):
+#         print('max value key: ', key)
+
+#4. Sort dictionary by keys.
+
+
+#5. Sort dictionary by values.
+
+#6. Merge two dictionaries.
+
+#7. Convert 2 lists → dictionary (zip).
+
+#8. Invert dictionary (swap keys and values).
+
+#9. Nested dictionary access (student records).
+
+#10. Check if two dictionaries are equal.
