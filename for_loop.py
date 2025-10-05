6a7ba77b76878557780aad8f0ff60f7fbde55e11
subodhghonge <subodhghonge12@gmail.com>
2025-10-05 14:51:51 +0530

    For Loops Practice

--- for_loop.py
+++ for_loop.py
@@ -108,14 +108,104 @@
 #         print(j, end=" ")
 #     print()
 
-#7) to print the pattern using for loop
-# * * * * *
-# * * * *
-# * * *
-# * *
-# *
+# #7) to print the pattern using for loop
+# # * * * * *
+# # * * * *
+# # * * *
+# # * *
+# # *
 
-for i in range(5, 0, -1):
-    for i in range(i):
-        print("*", end=" ")
-    print()
+# for i in range(5, 0, -1):
+#     for i in range(i):
+#         print("*", end=" ")
+#     print()
+
+# ## Problems
+# #1) fibonacci series
+
+# fs = 7
+# a = 0
+# b = 1
+
+# for i in range(fs):
+#     print(a, end=" ")
+#     a, b = b, a+b
+
+# #2) Reverse a Number
+# n = 12345
+# reverse = 0
+
+# length = len(str(n))
+
+# for i in range(length):
+#     length = n % 10
+#     reverse = reverse * 10 + length
+#     n = n // 10
+
+# print(reverse)
+
+# #3)Sum of digits
+# n = 12546
+# sum = 0
+
+# for i in range(n):
+#     r = n % 10
+#     sum += r
+#     n = n // 10
+
+# print(sum)
+
+# #4) Armstrong Number
+# n = 153
+# temp = n
+# power = len(str(n))
+# sum = 0
+
+# for i in range(n):
+#     r = n % 10
+#     sum = sum + (r**power)
+#     n = n // 10
+
+# print(sum)
+
+# #5) Chech number is palindrom or not
+# n = 12321
+# temp = n
+# reverse = 0
+
+# while n>0:
+#     r = n % 10
+#     reverse = reverse * 10 + r
+#     n = n // 10
+
+# print(reverse)
+# if temp == reverse:
+#     print("Palindrom")
+# else:
+#     print("Not a Palindrom")
+
+# #6) Prime Number
+
+# n = int(input("Enter a Number : "))
+
+# if n <= 1:
+#     print(f"{n} is not a prime number")
+# else:
+#     for i in range(2, int(n**0.5)+1):
+#         if n%i==0:
+#             print("Not a prime number")
+#         else:
+#             print("Prime Number")
+
+# #7) Perfect Number
+
+# n = int(input("Enter Number : "))
+# sum = 0
+# for i in range(1, n):
+#     if n%i==0:
+#         sum = sum + i
+
+# if sum == n:
+#     print("Perfect Number")
+# else:
+#     print("Not a Perfect Number")
