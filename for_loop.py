50d004b686821ebce123d4fbfbd67aeb0defa96a
subodhghonge <subodhghonge12@gmail.com>
2025-10-05 12:13:13 +0530

    For Loops Practice

---
+++ for_loop.py
@@ -0,0 +1,32 @@
+# #1. Print Numbers from 1 - 10
+# for i in range(1, 11):
+#     print(i, end=" ")
+
+# #2. Print Even numbers till 20
+# for i in range(0, 21, 2):
+#     print(i, end=" ")
+
+# #OR
+# for i in range(0, 21):
+#     if i%2==0:
+#         print(i)
+
+# #3. Print Odd number till 20
+# print("Odd Numbers are :")
+# for i in range(1, 21, 2):
+#     print(i, end=" ")
+
+# #OR
+# for i in range(0, 21):
+#     if i%2!=0:
+#         print(i, end=" ")
+
+# #4. print sqaure of numbers till 20
+# for i in range(1, 21):
+#     print(f"Square of {i} is : {i**2}")
+
+# #5. Cube of numbers till 20
+# for i in range(0, 21):
+#     print(f"Cube of number {i} is : {i**3}")
+
+#6. 
